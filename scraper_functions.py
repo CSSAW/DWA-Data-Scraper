905efcdd97e03d2df56b7d1c9672428813a02aa0
Brendan Khoury <you@example.com>
2020-06-04 16:00:06 -0700

    Added the flow data downloader, had to implement some error handling because a significant amount of data was non-exsitant

--- scraper_functions.py
+++ scraper_functions.py
@@ -40,29 +40,58 @@ def getStationData(stationInterfaceURL, alwaysDownload=False, storeData=False,  
     # lattitude:the lattitude of the station
     # longitude:The longitude of the station 
     # station:The name of the station
-
+    outputDict = {}
     stationInferface = documentDownloader(stationInterfaceURL,alwaysDownload=alwaysDownload, storeData=storeData)
     baseURL = stationInterfaceURL[:stationInterfaceURL.rfind("/")+1]
+    outputDict["lat"] = stationInferface.find(id="tbLat").get("value")
+    outputDict["long"] = stationInferface.find(id="tbLong").get("value")
+    outputDict["station"]=stationInferface.find(id="tbStation").get("value")
+    # lattitude = stationInferface.find(id="tbLat").get("value")
+    # longitude = stationInferface.find(id="tbLong").get("value")
+    # stationName = stationInferface.find(id="tbStation").get("value")
 
-    lattitude = stationInferface.find(id="tbLat").get("value")
-    longitude = stationInferface.find(id="tbLong").get("value")
-    stationName = stationInferface.find(id="tbStation").get("value")
-
-    # The construction of the URL below was created by analysing the output of a 
-    stationDataURL = baseURL + "HyData.aspx?Station=" + stationInferface.find(id="tbStation").get("value") +stationInferface.find("input", {"name":"ctl05"}).get("value") \
+    # The construction of the URL below was created by analysing the output of a form
+    stationDataURLPrimary = baseURL + "HyData.aspx?Station=" + stationInferface.find(id="tbStation").get("value") +stationInferface.find("input", {"name":"ctl05"}).get("value") \
     + "&DataType=Point&StartDT=" + stationInferface.find("input", {"name":"ctl06"}).get("value") \
     + "&EndDT=" + stationInferface.find("input", {"name":"tbEnd_0"}).get("value") \
     + "&SiteType=" + stationInferface.find("input", {"name":"tbSiteType"}).get("value")
 
-    stationData = documentDownloader(stationDataURL,alwaysDownload=alwaysDownload, storeData=storeData) 
+    stationDataPrimary = documentDownloader(stationDataURLPrimary,alwaysDownload=alwaysDownload, storeData=storeData) 
     # Remove any non-table items from the webPage. 
-    stationDataStr = stationData.find("pre").get_text()
-    substrStartKeyword = "Surface Water Level\n"
-    stationDataStr = stationDataStr[stationDataStr.find(substrStartKeyword)+len(substrStartKeyword):]
+    foundPre = stationDataPrimary.find("pre")
+    if foundPre != None:     
+        stationDataStrPrimary = foundPre.get_text()
+        substrStartKeyword = "Surface Water Level\n"
+        stationDataStrPrimary = stationDataStrPrimary[stationDataStrPrimary.find(substrStartKeyword)+len(substrStartKeyword):]
 
-    # Use a stringIo object to use the pandas read_fwf function. 
-    # This converts a pure text table with a bunch of columns to a pandas dataframe. 
-    strBuffer = io.StringIO(stationDataStr)
-    stationDataDf = pd.read_fwf(strBuffer)
-    strBuffer.close()
-    return {"df": stationDataDf, "lat": lattitude, "long":longitude,"station":stationName}
+        # Use a stringIo object to use the pandas read_fwf function. 
+        # This converts a pure text table with a bunch of columns to a pandas dataframe. 
+        strBuffer = io.StringIO(stationDataStrPrimary)
+        outputDict["dfPrimary"] = pd.read_fwf(strBuffer)
+        strBuffer.close()
+    else:
+        outputDict["dfPrimary"] = None
+    
+    # The construction of the URL below was created by analysing the output of a form 
+    stationDataURLFlow = baseURL + "HyData.aspx?Station=" + stationInferface.find(id="tbStation").get("value") +stationInferface.find("input", {"name":"ctl05"}).get("value") \
+    + "&DataType=Daily&StartDT=" + stationInferface.find("input", {"name":"ctl06"}).get("value") \
+    + "&EndDT=" + stationInferface.find("input", {"name":"tbEnd_0"}).get("value") \
+    + "&SiteType=" + stationInferface.find("input", {"name":"tbSiteType"}).get("value")
+
+    stationDataFlow = documentDownloader(stationDataURLFlow,alwaysDownload=alwaysDownload, storeData=storeData) 
+    # Remove any non-table items from the webPage. 
+    foundPre = stationDataFlow.find("pre")
+    if foundPre != None:       
+        stationDataStrFlow = foundPre.get_text()
+        substrStartKeyword = "DATE     D AVG F/R  QUAL"
+        stationDataStrFlow = stationDataStrFlow[stationDataStrFlow.find(substrStartKeyword):]
+
+        # Use a stringIo object to use the pandas read_fwf function. 
+        # This converts a pure text table with a bunch of columns to a pandas dataframe. 
+        strBuffer = io.StringIO(stationDataStrFlow)
+        outputDict["dfFlow"] = pd.read_fwf(strBuffer)
+        strBuffer.close()
+    else:
+        outputDict["dfFlow"] = None
+
+    return outputDict#{"dfPrimary": stationDataPrimaryDf, "dfFlow":stationDataFlowDf, "lat": lattitude, "long":longitude,"station":stationName}
